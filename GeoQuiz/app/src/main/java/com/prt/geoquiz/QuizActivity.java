5ee563f8e0756bd06b429d59fc45059d646d47d2
oleno <pirit.lena@gmail.com>
2017-02-07 11:02:01 +0200

    applying of the listeners for True and False buttons

--- GeoQuiz/app/src/main/java/com/prt/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/prt/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.prt.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class QuizActivity extends AppCompatActivity {
@@ -15,6 +16,20 @@ public class QuizActivity extends AppCompatActivity {
         setContentView(R.layout.activity_quiz);
 
         mTrueButton = (Button) findViewById(R.id.true_button);
+        // listeners (for True and False buttons) implemented like inner anonymous class
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // ToDo: handling
+            }
+        });
+
         mFalseButton = (Button) findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // ToDo: handling
+            }
+        });
     }
 }
