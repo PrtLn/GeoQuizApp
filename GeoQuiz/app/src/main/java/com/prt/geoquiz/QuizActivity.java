55ddc55fac141673966194c2851f8d469bd07360
oleno <pirit.lena@gmail.com>
2017-02-07 12:43:24 +0200

    encapsulation in the updateQuestion method

--- GeoQuiz/app/src/main/java/com/prt/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/prt/geoquiz/QuizActivity.java
@@ -24,14 +24,17 @@ public class QuizActivity extends AppCompatActivity {
 
     private int mCurrentIndex = 0;
 
+    private void updateQuestion() {
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
         mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
-        int question = mQuestionBank[mCurrentIndex].getTextResId();
-        mQuestionTextView.setText(question);
 
         mTrueButton = (Button) findViewById(R.id.true_button);
         // listeners (for True and False buttons) implemented like inner anonymous class
@@ -59,9 +62,10 @@ public class QuizActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
-                int question = mQuestionBank[mCurrentIndex].getTextResId();
-                mQuestionTextView.setText(question);
+                updateQuestion();
             }
         });
+
+        updateQuestion();
     }
 }
