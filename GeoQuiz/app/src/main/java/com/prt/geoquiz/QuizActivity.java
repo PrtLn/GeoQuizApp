870737e568c1dc7e593cc732c07fb84a88b6175e
oleno <pirit.lena@gmail.com>
2017-02-07 15:16:01 +0200

    adding the Prev(ious) button and images

--- GeoQuiz/app/src/main/java/com/prt/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/prt/geoquiz/QuizActivity.java
@@ -12,6 +12,7 @@ public class QuizActivity extends AppCompatActivity {
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
+    private Button mPrevButton;
     private TextView mQuestionTextView;
 
     private Question[] mQuestionBank = new Question[] {
@@ -50,7 +51,7 @@ public class QuizActivity extends AppCompatActivity {
 
         mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
 
-        mTrueButton = (Button) findViewById(R.id.true_button);
+        mTrueButton = (Button)findViewById(R.id.true_button);
         // listeners (for True and False buttons) implemented like inner anonymous class
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -72,6 +73,15 @@ public class QuizActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+
+        mPrevButton = (Button)findViewById(R.id.prev_button);
+        mPrevButton.setOnClickListener(new  View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex - 1) % mQuestionBank.length;
                 updateQuestion();
             }
         });
