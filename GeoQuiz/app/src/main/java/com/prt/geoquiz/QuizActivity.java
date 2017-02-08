150d6b8bb1e0d782c270377025103772e0b57df2
oleno <pirit.lena@gmail.com>
2017-02-08 16:13:27 +0200

    creating a landscape layout

--- GeoQuiz/app/src/main/java/com/prt/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/prt/geoquiz/QuizActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,8 +14,8 @@ public class QuizActivity extends AppCompatActivity {
 
     private Button mTrueButton;
     private Button mFalseButton;
-    private ImageButton mNextButton;
-    private ImageButton mPrevButton;
+    private Button mNextButton;
+    private Button mPrevButton;
     private TextView mQuestionTextView;
 
     private Question[] mQuestionBank = new Question[] {
@@ -74,7 +73,7 @@ public class QuizActivity extends AppCompatActivity {
             }
         });
 
-        mNextButton = (ImageButton)findViewById(R.id.next_button);
+        mNextButton = (Button)findViewById(R.id.next_button);
         mNextButton.setOnClickListener(new  View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -83,7 +82,7 @@ public class QuizActivity extends AppCompatActivity {
             }
         });
 
-        mPrevButton = (ImageButton)findViewById(R.id.prev_button);
+        mPrevButton = (Button)findViewById(R.id.prev_button);
         mPrevButton.setOnClickListener(new  View.OnClickListener() {
             @Override
             public void onClick(View v) {
