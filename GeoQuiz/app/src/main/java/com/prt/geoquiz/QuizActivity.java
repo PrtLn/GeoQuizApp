2f2908a01721999e48fd87e2fecfee685e4bbd0e
oleno <pirit.lena@gmail.com>
2017-02-09 09:17:00 +0200

    starting an activity (communicating with intent)

--- GeoQuiz/app/src/main/java/com/prt/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/prt/geoquiz/QuizActivity.java
@@ -1,5 +1,6 @@
 package com.prt.geoquiz;
 
+import android.content.Intent;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -98,6 +99,8 @@ public class QuizActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // start CheatActivity
+                Intent intent = new Intent(QuizActivity.this, CheatActivity.class);
+                startActivity(intent);
             }
         });
 
